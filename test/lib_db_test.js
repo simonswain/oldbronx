c1cf6d0f3157c181b084be48b6167bf40ba56b75
Simon Swain <simon@modus.com.au>
2013-05-02 17:37:21 +1000

    Database api tests

--- test/lib_db_test.js
+++ test/lib_db_test.js
@@ -1,43 +1,43 @@
 "use strict";
 
-// var api = require('../lib/bronx.js').api;
+var db = require('../lib/bronx').api.db;
 
-// exports.devices = {
-//   'callback-query': function(test) {    
-//     api.db.conn(
-//       function(err, conn){
-//         conn.query(
-//           'SELECT NOW()', 
-//           function (err, res) {
-//             test.equals(err, null, 'no error');
-//             test.equals(res.rows.length, 1, '1 row');
-//             conn.end();
-//             test.done();              
-//           });
-//       });
-//   },
-//   'pool-query': function(test) {    
-//     api.db.pool.acquire(
-//       function(err, conn){
-//         var query = conn.query('SELECT pg_get_keywords()');
-//         var res = [];
+exports.devices = {
+  'callback-query': function(test) {    
+    db.conn(
+      function(err, conn){
+        conn.query(
+          'SELECT NOW()', 
+          function (err, res) {
+            test.equals(err, null, 'no error');
+            test.equals(res.rows.length, 1, '1 row');
+            conn.end();
+            test.done();              
+          });
+      });
+  },
+  'pool-query': function(test) {    
+    db.pool.acquire(
+      function(err, conn){
+        var query = conn.query('SELECT pg_get_keywords()');
+        var res = [];
 
-//         query.on('row', function(x) {
-//           res.push(x);
-//         });
+        query.on('row', function(x) {
+          res.push(x);
+        });
 
-//         // query.on('error', function(err) {
-//         //   test.equals(err, null, 'no error');
-//         //   test.ok(false, 'got error from query');
-//         //   db.pool.release(conn);
-//         //   test.done();
-//         // });
+        query.on('error', function(err) {
+          test.equals(err, null, 'no error');
+          test.ok(false, 'got error from query');
+          db.pool.release(conn);
+          test.done();
+        });
 
-//         query.on('end', function() {
-//           test.ok(true, 'got results from query');
-//           api.db.pool.release(conn);
-//           test.done();
-//         });
-//       });
-//   }
-// };
+        query.on('end', function() {
+          test.ok(true, 'got results from query');
+          db.pool.release(conn);
+          test.done();
+        });
+      });
+  }
+};
